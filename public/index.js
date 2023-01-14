e8a558ad57382ff3d9ba2bdbcfeec25fd123929d
annielam0623 <annzhouyy@gmail.com>
2023-01-13 23:08:24 -0800

    finish line chart
    the the stock value got erro

--- public/index.js
+++ public/index.js
@@ -1,23 +1,19 @@
 async function main(){
-   const response = await fetch('https://api.twelvedata.com/time_series?symbol=AAPL,GME,DIS,BTNX&interval=1min&apikey=3277e72390bf42ac8156df421f8ee63d');
+   const response = await fetch("https://api.twelvedata.com/time_series?symbol=AAPL,GME,DIS,BNTX&interval=1min&apikey=3277e72390bf42ac8156df421f8ee63d");
     const result = await response.json();
 
-    const {AAPL, GME, DIS, BNTX} = mockData;
-    // let AAPL = result.AAPL
-    // let GME = result.GME
-    // let DIS = result.DIS
-    // let BTNX = result.BTNX
-// Bonus Note: 
-// Another way to write the above lines would to refactor it as:
-   // const {AAPL, GME, DIS, BTNX} = result 
-// This is an example of "destructuring" an object
-// "Destructuring" creates new variables from an object or an array
-    
+    // const {AAPL, GME, DIS, BNTX} = mockData;
+
+    let AAPL = result.AAPL
+    let GME = result.GME
+    let DIS = result.DIS
+    let BNTX = result.BNTX
+
     const stocks = [AAPL, GME, DIS, BNTX];
  
-    const timeChartCanvas = document.getElementById('#time-chart');
-    const highestPriceChartCanvas = document.getElementById('#highest-price-chart');
-    const averagePriceChartCanvas = document.getElementById('#average-price-chart');
+    const timeChartCanvas = document.querySelector('#time-chart');
+    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
     //set stock symbo into different color
     function getColor(stock){
@@ -34,27 +30,27 @@ async function main(){
             return 'rgba(166, 43, 158, 0.7)'
         }
     }
+
     // line chart
-    stocks.forEach(stock => stock.values.reverse());
-    new Chart(timeChartCanvas.getContext('2d'), {
+    stocks.forEach((stock) => stock.values.reverse());
+    new Chart(timeChartCanvas.getContext("2d"), {
     type: "line",
     data: {
         labels: stocks[0].values.reverse().map((value) => value.datetime),
         datasets: stocks.map((stock) => ({
         label: stock.meta.symbol,
-        data: stock.values.reverse().map((value) => parseFloat(value.high)),
+        data: stock.values.map((value) => parseFloat(value.high)),
         backgroundColor: getColor(stock.meta.symbol),
         borderColor: getColor(stock.meta.symbol),
-            }))
+            })),
         },
         options: {
             scales: {
               y: {
                 beginAtZero: true
-              }
-            }
-        }      
+              },
+            },
+        },   
     });
-
 }
-main();
+main()
